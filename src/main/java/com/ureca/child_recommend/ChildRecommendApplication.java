fddf6c0641a34d359cf6511352ebf7f1acbbb18a
jungjoo <wjddis000507@gmail.com>
2024-10-11 13:29:03 +0900

    2024.10.11 start

---
+++ src/main/java/com/ureca/child_recommend/ChildRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.ureca.child_recommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildRecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildRecommendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ureca/child_recommend/ChildRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ureca.child_recommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
