37c3341406c7dda904bad21a01ef882ca290af11
Shanate <redeyes5240@gmail.com>
2024-10-19 15:22:34 +0900

    [test/9] SSE 연결에 대한 로그 추가

--- src/main/java/com/ureca/child_recommend/notice/application/SseEmitterManager.java
+++ src/main/java/com/ureca/child_recommend/notice/application/SseEmitterManager.java
@@ -1,6 +1,7 @@
 package com.ureca.child_recommend.notice.application;
 
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -13,12 +14,19 @@ public class SseEmitterManager {
 
 
     public SseEmitter createEmitter(){
-        SseEmitter emitter = new SseEmitter(3600 * 1000L); // 1시간
+//        SseEmitter emitter = new SseEmitter(3600 * 1000L); // 1시간
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE); // Endless Connection
         emitters.add(emitter);
 
         // 클라이언트 연결 종료 시 제거
-        emitter.onCompletion(() -> emitters.remove(emitter));
-        emitter.onTimeout(() -> emitters.remove(emitter));
+        emitter.onCompletion(() -> {
+            emitters.remove(emitter);
+            System.out.println("Emitter completed and removed.");
+        });
+        emitter.onTimeout(() -> {
+            emitters.remove(emitter);
+            System.out.println("Emitter timed out and removed.");
+        });
 
         return emitter;
     }
@@ -29,6 +37,19 @@ public class SseEmitterManager {
                 emitter.send(notification, MediaType.TEXT_EVENT_STREAM);
             } catch (Exception e){
                 emitters.remove(emitter);
+            }
+        }
+    }
+
+    @Scheduled(fixedRate = 120000) // 2 분마다 heartbeat 전송
+    public void sendHeartbeat() {
+        for (SseEmitter emitter : emitters) {
+            try {
+                emitter.send("heartbeat", MediaType.TEXT_EVENT_STREAM);
+                System.out.println("Sent heartbeat to emitter."); // 로그 추가
+            } catch (Exception e) {
+                emitters.remove(emitter);  // 실패 시 emitter 제거
+                System.err.println("Error sending heartbeat: " + e.getMessage()); // 오류 로그 추가
             }
         }
     }
