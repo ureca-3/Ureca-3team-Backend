d6edd5d45661939ce75de8a1bb05575665a835be
hyewonbae <bhwn98@gmail.com>
2024-10-20 19:11:45 +0900

    [feature/8] MBTI 진단 삭제(물리적)

--- src/main/java/com/ureca/child_recommend/child/domain/presentation/dto/MbtiDto.java
+++ src/main/java/com/ureca/child_recommend/child/domain/presentation/dto/MbtiDto.java
@@ -1,21 +1,31 @@
 package com.ureca.child_recommend.child.domain.presentation.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 
-@Getter
-@Builder
-@AllArgsConstructor
-@Data
 public class MbtiDto {
-    private int M;
-    private int B;
-    private int T;
-    private int I;
 
-    private String mbti1, mbti2, mbti3, mbti4;
+    public static class Response{
+        @Builder
+        @Getter
+        public static class assessmentMbtiDto{
+            private String result;
 
-    private String result;
+            public static assessmentMbtiDto of(String result){
+                return assessmentMbtiDto.builder()
+                        .result(result)
+                        .build();
+            }
+        }
+
+    }
+    public static class Request{
+        @Getter
+        public static class assessmentMbtiDto {
+            private int m;
+            private int b;
+            private int t;
+            private int i;
+        }
+    }
 }
