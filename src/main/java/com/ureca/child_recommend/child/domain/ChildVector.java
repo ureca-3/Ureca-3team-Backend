ed661ca23aec12756286731c55ce6e19fee55499
jungjoo <wjddis000507@gmail.com>
2024-10-27 03:10:02 +0900

    [refactor] mysql -> postgresql 변경/pgvector 확장
    - postgresql user는 예약어라 users로 수정
    - postgresql vector 디비 사용을 위한 pgvector 확장

---
+++ src/main/java/com/ureca/child_recommend/child/domain/ChildVector.java
@@ -0,0 +1,44 @@
+package com.ureca.child_recommend.child.domain;
+
+import com.ureca.child_recommend.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Array;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChildVector extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "child_vector_id")
+    private Long id;
+
+    @JdbcTypeCode(SqlTypes.VECTOR)
+    @Array(length = 1536)
+    private float[] embedding;
+
+    @OneToOne
+    @JoinColumn(name = "child_id")
+    private Child childId;
+
+
+
+    public static ChildVector createChildVector(float[] childEmbedding,Child child){
+        return ChildVector.builder()
+                .embedding(childEmbedding)
+                .childId(child)
+                .build();
+
+    }
+
+}
