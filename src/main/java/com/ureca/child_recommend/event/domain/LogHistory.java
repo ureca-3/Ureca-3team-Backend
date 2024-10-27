4b897657ff708ff7c3fc5c4687f160e9ecdcfb6a
ChoiJiHooon <prin81208@naver.com>
2024-10-27 17:51:20 +0900

    [feature/13]Entity 설정

---
+++ src/main/java/com/ureca/child_recommend/event/domain/LogHistory.java
@@ -0,0 +1,43 @@
+package com.ureca.child_recommend.event.domain;
+
+import com.ureca.child_recommend.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class LogHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "log_id")
+    private Long id;
+
+    @Column
+    private LocalDateTime log;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+
+
+}
