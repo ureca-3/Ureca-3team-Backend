5ba78228f79e8b30eb015f3a2fbc040ccb2c0cc8
jungjoo <wjddis000507@gmail.com>
2024-10-29 14:33:15 +0900

    [feature/16] 해당 자녀의 진단 내역 조회 API
    - childMbti에 일대일연결로 childMbtiScore추가 : 삭제시 -> ChildMbti/Score DELETE 상태 변경, 재진단시 ->  ChildMbti/Score NONACTIVE 상태 변경, 피드백시 -> 오늘 날짜의 ACTIVE된 childMbtiScore 값이 update, 결과가 바뀌면 ChildMbti의 값이 update 되므로, 1:1 관계이므로 OnetoOne으로 변경

--- src/main/java/com/ureca/child_recommend/child/presentation/dto/MbtiDto.java
+++ src/main/java/com/ureca/child_recommend/child/presentation/dto/MbtiDto.java
@@ -1,10 +1,13 @@
 package com.ureca.child_recommend.child.presentation.dto;
 
+import com.ureca.child_recommend.child.domain.ChildMbtiScore;
+import com.ureca.child_recommend.child.domain.Enum.ChildMbtiScoreStatus;
 import lombok.Builder;
 import lombok.Getter;
 
-public class MbtiDto {
+import java.time.LocalDate;
 
+public class MbtiDto {
     public static class Response{
         @Builder
         @Getter
@@ -17,6 +20,32 @@ public class MbtiDto {
                         .build();
             }
         }
+
+        @Builder
+        @Getter
+        public static class assessmentMbtiResultDto{
+            private Long childMbtiScoreId;
+            private LocalDate assessmentDate;
+            private int eiScore;
+            private int jpScore;
+            private int snScore;
+            private int tfScore;
+            private String mbtiResult;
+            private ChildMbtiScoreStatus status;
+            public static assessmentMbtiResultDto of(ChildMbtiScore childMbtiScore, String mbtiResult){
+                return assessmentMbtiResultDto.builder()
+                        .childMbtiScoreId(childMbtiScore.getId())
+                        .assessmentDate(childMbtiScore.getAssessmentDate())
+                        .eiScore(childMbtiScore.getEiScore())
+                        .jpScore(childMbtiScore.getJpScore())
+                        .snScore(childMbtiScore.getSnScore())
+                        .tfScore(childMbtiScore.getTfScore())
+                        .mbtiResult(mbtiResult)
+                        .status(childMbtiScore.getStatus())
+                        .build();
+            }
+        }
+
 
     }
     public static class Request{
